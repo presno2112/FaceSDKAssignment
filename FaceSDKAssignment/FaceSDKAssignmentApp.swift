58466c93e89dec3e74e040d340634e64f2a9e8da
Sebastián Presno Alvarado <93207034+presno2112@users.noreply.github.com>
2025-04-22 21:04:42 +0200

    Initial Commit

---
+++ FaceSDKAssignment/FaceSDKAssignmentApp.swift
@@ -0,0 +1,17 @@
+//
+//  FaceSDKAssignmentApp.swift
+//  FaceSDKAssignment
+//
+//  Created by Sebastian Presno on 22/04/25.
+//
+
+import SwiftUI
+
+@main
+struct FaceSDKAssignmentApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
